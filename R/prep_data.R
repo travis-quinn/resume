b54db43de11175201ad1b91b8f997ab7bc749dfd
Chuck Horgan <chorgan182@gmail.com>
2023-02-25 18:42:18 -0500

    Outline cleaning and yaml writing functions

--- R/prep_data.R
+++ R/prep_data.R
@@ -7,3 +7,90 @@
 # func for writing to yaml _variables
 
 # func for heatmap
+
+### TEMP
+DATA <- get_data("1DrQduf2C0muSsZFNJI5y9Ljtr3qKYNacC9YRkI8NiQs")
+
+
+
+
+
+write_to_vars <- function(DATA) {
+
+  nested_list <- list(
+    contact = clean_contact(DATA$contact),
+    work = clean_work(DATA$work),
+    work_other = clean_work_other(DATA$work_other),
+    education = clean_education(DATA$education),
+    avocations = clean_avocations(DATA$avocations)
+  )
+
+  yaml::write_yaml(nested_list, "_variables.yml")
+
+}
+
+
+
+clean_contact <- function(df) {
+
+  rlang::set_names(df$text,
+                   nm = df$id) %>%
+    as.list() %>%
+    list(contact = .)
+
+}
+
+
+
+clean_work <- function(df) {
+
+  work_cols <- names(df) %>%
+    purrr::keep(~ stringr::str_detect(.x, "^work"))
+
+  purrr::set_names(work_cols) %>%
+    purrr::map(~ {
+      df_work <- df %>%
+        dplyr::select(id, all_of(.x)) %>%
+        dplyr::filter(!is.na(.data[[.x]]))
+      rlang::set_names(df_work[[.x]],
+                       nm = df_work[["id"]]) %>%
+        as.list()
+    })
+
+}
+
+
+
+clean_work_other <- function(df) {
+
+
+
+}
+
+
+
+clean_education <- function(df) {
+
+
+
+}
+
+
+
+clean_skills <- function(df) {
+
+
+
+}
+
+
+
+clean_avocations <- function(df) {
+
+
+
+}
+
+
+
+
