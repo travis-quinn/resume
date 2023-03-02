3d1bd90f0b7d270f0ec58dd7d55269ada33a883b
Chuck Horgan <chorgan182@gmail.com>
2023-03-02 18:57:29 -0500

    Adjust skills graphic

--- R/heatmap.R
+++ R/heatmap.R
@@ -14,9 +14,12 @@ create_skills_heatmap <- function(skills_data) {
 
   ggplot2::ggplot(skills_data, ggplot2::aes(x = Skill, y = Software, fill = Level)) +
     ggplot2::geom_tile(color = "white",
-                       lwd = 1,
+                       lwd = 0.3,
                        linetype = 1) +
-    ggplot2::geom_text(ggplot2::aes(label = Level), size = 3, na.rm = TRUE) +
+    ggplot2::geom_text(ggplot2::aes(label = Level),
+                       size = 3,
+                       fontface = "bold",
+                       na.rm = TRUE) +
     #ggplot2::coord_fixed() +
     ggplot2::theme_minimal() +
     ggplot2::scale_fill_brewer(type = "seq",
@@ -27,31 +30,49 @@ create_skills_heatmap <- function(skills_data) {
                                breaks = exp_level_levels,
                                labels = exp_level_descriptions) +
     ggplot2::scale_x_discrete(position = "top",
-                              guide = ggplot2::guide_axis(n.dodge = 2),
+                              # guide = ggplot2::guide_axis(n.dodge = 2),
                               labels = scales::label_wrap(10),
                               expand = c(0, 0)) +
     ggplot2::scale_y_discrete(labels = scales::label_wrap(10),
                               expand = c(0, 0)) +
-    ggplot2::theme(legend.position = "bottom",
-                   axis.ticks.length = ggplot2::unit(0, "cm"),
-                   panel.grid = ggplot2::element_blank(),
-                   panel.border = ggplot2::element_blank(),
-                   text = ggplot2::element_text(size = 9),
-                   # axis.title.y = ggplot2::element_text(
-                   #   size = 10,
-                   #   face = "bold",
-                   #   margin = ggplot2::margin(l = 0, r = -30,
-                   #                            t = -70, b = 0),
-                   #   hjust = 1,
-                   #   angle = 0
-                   # )),
-                   axis.title.y = ggplot2::element_blank(),
-                   # axis.title.x = ggplot2::element_text(
-                   #   size = 10,
-                   #   face = "bold"
-                   # )
-                   axis.title.x = ggplot2::element_blank()) +
-    ggplot2::guides(fill = ggplot2::guide_legend(direction = "vertical"))
+    ggplot2::theme(
+      legend.position = "bottom",
+      legend.spacing.y = ggplot2::unit(0, "cm"),
+      legend.title = ggplot2::element_text(margin = ggplot2::margin(b = 3)),
+      legend.margin = ggplot2::margin(t = -5),
+      legend.key.size = ggplot2::unit(0.5, "cm"),
+      legend.justification = "left",
+      legend.background = ggplot2::element_rect(fill = 'transparent', color = NA),
+      legend.box.background = ggplot2::element_rect(fill = 'transparent', color = NA),
+      axis.ticks.length = ggplot2::unit(0, "cm"),
+      panel.grid = ggplot2::element_blank(),
+      panel.border = ggplot2::element_blank(),
+      text = ggplot2::element_text(size = 10),
+      # axis.title.y = ggplot2::element_text(
+      #   size = 10,
+      #   face = "bold",
+      #   margin = ggplot2::margin(l = 0, r = -30,
+      #                            t = -70, b = 0),
+      #   hjust = 1,
+      #   angle = 0
+      # )),
+      axis.title.y = ggplot2::element_blank(),
+      # axis.title.x = ggplot2::element_text(
+      #   size = 10,
+      #   face = "bold"
+      # )
+      axis.text.x.top = ggplot2::element_text(angle = 20, vjust = 0.6, hjust = 0.5),
+      # it needed to be axis.text.x.TOP - we were missing the TOP part because
+      #   vjust acts differently if the axis text is on top rather than the bottom
+      ###^ :)
+      axis.title.x = ggplot2::element_blank(),
+      panel.background = ggplot2::element_rect(fill = 'transparent'),
+      plot.background = ggplot2::element_rect(fill = 'transparent', color = NA),
+      panel.grid.major = ggplot2::element_blank(),
+      panel.grid.minor = ggplot2::element_blank()
+    ) +
+    ggplot2::guides(fill = ggplot2::guide_legend(direction = "vertical",
+                                                 byrow = TRUE))
 
 }
 
@@ -60,10 +81,10 @@ create_skills_heatmap <- function(skills_data) {
 save_skills_heatmap <- function(p, path) {
 
   ggplot2::ggsave(plot = p, filename = path,
-                  bg = "white",
+                  bg = "transparent",
                   device = "svg",
-                  width = 0.9 * 0.4 * 8.5,
-                  height = 6.5,
+                  width = 370 / 96,
+                  height = 600 / 96,
                   units = "in")
 
 }
